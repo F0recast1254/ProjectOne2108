936b09b8f97de91a294b6ed1fe7f3cc4db72e0ef
SpecOpsRising <113926501+SpecOpsRising@users.noreply.github.com>
2022-09-23 18:03:47 -0400

    Update Request.java

--- Request.java
+++ Request.java
@@ -2,6 +2,7 @@ public class Request {
     String ID;
     long arrivalTime;
     double priority;
+    Request next;
     Request(){}
     Request(String ID, long arrivalTime, double priority){
         this.ID = ID;
